4687c805ab3647352baaecce38f7bf85914b4966
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-02-15 20:33:35 -0800

    Updates

---
+++ precache-manifest.49a0609978dba4f1d62b2c2608716195.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb6d0672d22f0a4a035de7527217924",
+    "url": "/caeser-cipher/index.html"
+  },
+  {
+    "revision": "c3a64913be463327b6d1",
+    "url": "/caeser-cipher/static/css/main.fe22ed49.chunk.css"
+  },
+  {
+    "revision": "440fe1863bf7c6e084c0",
+    "url": "/caeser-cipher/static/js/2.8701a7a8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/caeser-cipher/static/js/2.8701a7a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a64913be463327b6d1",
+    "url": "/caeser-cipher/static/js/main.75b73c8d.chunk.js"
+  },
+  {
+    "revision": "398315f8716a9afb626a",
+    "url": "/caeser-cipher/static/js/runtime-main.acf32bf4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/carlosdlroca.github.io/caeser-cipher/precache-manifest.2fbd8f931be04bf28939b094e540246c.js"
+  "/caeser-cipher/precache-manifest.49a0609978dba4f1d62b2c2608716195.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/carlosdlroca.github.io/caeser-cipher/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/caeser-cipher/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
