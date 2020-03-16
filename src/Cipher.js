b81150e3eb402f74ebdf5abcd398557843d8011b
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-03-15 18:46:03 -0700

    Extract encrypt function into its own file

--- src/Cipher.js
+++ src/Cipher.js
@@ -1,33 +1,12 @@
 import React, { useState } from "react";
 import Input from "./Input";
 import Shift from "./Shift";
+import { encrypt } from "./utils/encrypt";
 
 export default () => {
     const [text, setText] = useState("");
     const [shiftAmount, setShift] = useState(0);
     const [encryptedText, setEncryptedText] = useState(text);
-    const ALPHA = /[a-zA-Z]/;
-
-    const encrypt = (value, shift) => {
-        let encrypted = "";
-        for (let char of value) {
-            encrypted += encryptCharacter(char, shift);
-        }
-        return encrypted;
-    };
-
-    const encryptCharacter = (char, shift) => {
-        const charCode = char.charCodeAt(0);
-        if (ALPHA.test(char)) {
-            let lower;
-            if (charCode >= 65 && charCode <= 90) lower = 65;
-            if (charCode >= 97 && charCode <= 122) lower = 97;
-
-            const encryptedCharCode = ((charCode - lower + shift) % 26) + lower;
-            return String.fromCharCode(encryptedCharCode);
-        }
-        return char;
-    };
 
     return (
         <section className='Cipher'>
