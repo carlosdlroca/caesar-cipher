e3e2bec0dd6c1b154e7ec7fa9a0d7e655a5eae94
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-03-15 18:47:47 -0700

    Refactor event handlers

--- src/Cipher.js
+++ src/Cipher.js
@@ -8,26 +8,28 @@ export default () => {
     const [shiftAmount, setShift] = useState(0);
     const [encryptedText, setEncryptedText] = useState(text);
 
+    const handleInputOnChange = e => {
+        const { value } = e.target;
+        setText(value);
+        setEncryptedText(encrypt(value, shiftAmount));
+    };
+
+    const handleOnShiftChange = e => {
+        const { value } = e.target;
+        setShift(Number(value));
+        setEncryptedText(encrypt(text, Number(value)));
+    };
+
     return (
         <section className='Cipher'>
             <Input
                 value={text}
                 placeholder={"Enter your text"}
-                onChange={e => {
-                    const { value } = e.target;
-                    setText(value);
-                    setEncryptedText(encrypt(value, shiftAmount));
-                }}
+                onChange={handleInputOnChange}
             />
-            <Shift
-                onChange={e => {
-                    const { value } = e.target;
-                    setShift(Number(value));
-                    setEncryptedText(encrypt(text, Number(value)));
-                }}
-            />
+            <Shift onChange={handleOnShiftChange} />
             <h3 className='EncryptedText'>
-                <span class='EncryptedLabel'>Encrypted:</span> <br />{" "}
+                <span class='EncryptedLabel'>Encrypted:</span> <br />
                 {encryptedText}
             </h3>
         </section>
